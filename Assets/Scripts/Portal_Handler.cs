8afd1c4305381077eb0f36c10d19b9524d98c29d
Metnias <40355580+metnias@users.noreply.github.com>
2023-02-14 11:16:09 +0900

    Begin to add room handling logics

---
+++ Assets/Scripts/Portal_Handler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ExitDirection
+{
+    Down,
+    Right,
+    Up,
+    Left
+}
+
+public class Portal_Handler : MonoBehaviour
+{
+    public string sceneName = "";
+    public int doorNumber = 0;
+    public ExitDirection direction = ExitDirection.Down;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) return;
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/Room_Manager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_Manager : MonoBehaviour
+{
+    public static int doorNumber = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
